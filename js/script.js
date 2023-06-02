4b9da43aa60062fa397ae80f88b36c6b7bef3269
Sage-2001 <somya.malgudi@gmail.com>
2023-06-02 09:41:05 +0530

    Updated createSave Button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const form = document.getElementById('generate-form');
-const qr = document.getElementById('qrcode');
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
 
 // Button submit
 const onGenerateSubmit = (e) => {
@@ -7,12 +7,12 @@ const onGenerateSubmit = (e) => {
 
   clearUI();
 
-  const url = document.getElementById('url').value;
-  const size = document.getElementById('size').value;
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
 
   // Validate url
-  if (url === '') {
-    alert('Please enter a URL');
+  if (url === "") {
+    alert("Please enter a URL");
   } else {
     showSpinner();
     // Show spinner for 1 sec
@@ -23,7 +23,7 @@ const onGenerateSubmit = (e) => {
       // Generate the save button after the qr code image src is ready
       setTimeout(() => {
         // Get save url
-        const saveUrl = qr.querySelector('img').src;
+        const saveUrl = qr.querySelector("img").src;
         // Create save button
         createSaveBtn(saveUrl);
       }, 50);
@@ -33,7 +33,7 @@ const onGenerateSubmit = (e) => {
 
 // Generate QR code
 const generateQRCode = (url, size) => {
-  const qrcode = new QRCode('qrcode', {
+  const qrcode = new QRCode("qrcode", {
     text: url,
     width: size,
     height: size,
@@ -42,8 +42,8 @@ const generateQRCode = (url, size) => {
 
 // Clear QR code and save button
 const clearUI = () => {
-  qr.innerHTML = '';
-  const saveBtn = document.getElementById('save-link');
+  qr.innerHTML = "";
+  const saveBtn = document.getElementById("save-link");
   if (saveBtn) {
     saveBtn.remove();
   }
@@ -51,35 +51,49 @@ const clearUI = () => {
 
 // hide  scanner
 const showScanner = () => {
-  const scanner = document.getElementById('qrCodeContainer');
-  scanner.style.display = 'block';
+  const scanner = document.getElementById("qrCodeContainer");
+  scanner.style.display = "block";
 };
 
 // Show spinner
 const showSpinner = () => {
-  const spinner = document.getElementById('spinner');
-  spinner.style.display = 'block';
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
 };
 
 // Hide spinner
 const hideSpinner = () => {
-  const spinner = document.getElementById('spinner');
-  spinner.style.display = 'none';
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
 };
 
 // Create save button to download QR code as image
 const createSaveBtn = (saveUrl) => {
-  const link = document.createElement('a');
-  link.id = 'save-link';
-  link.classList = 'bg-green-500 hover:bg-blue-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
-  link.innerHTML = 'Save Image';
-  link.href = saveUrl;
-  link.download = 'qrcode.png';
+  const button = document.createElement("button");
+  button.id = "save-button";
+  button.classList =
+    "bg-green-500 hover:bg-blue-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+  button.innerHTML = "Save Image";
 
-  document.getElementById('generated').appendChild(link);
+  button.addEventListener("click", () => {
+    downloadQRCode(saveUrl);
+  });
+
+  document.getElementById("generated").appendChild(button);
 };
 
+// Function to download the QR code image
+const downloadQRCode = (url) => {
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const a = document.createElement("a");
+      a.href = URL.createObjectURL(blob);
+      a.download = "qrcode.png";
+      a.click();
+    });
+};
 
 hideSpinner();
 
-form.addEventListener('submit', onGenerateSubmit);
+form.addEventListener("submit", onGenerateSubmit);
